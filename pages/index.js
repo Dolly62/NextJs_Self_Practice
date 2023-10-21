099de893ae5fb7e746c016235b1712b4760b2462
Dolly62 <kambojdolly798@gmail.com>
2023-10-21 14:59:38 +0530

    Navigation & Custom 404 page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home(){
-    return <h1>Home</h1>
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function Home() {
+  const router = useRouter();
+
+  const clickHandler = () => {
+    router.push("/product");
+  };
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/blog">Blog</Link> <br />
+      <Link href="/product">Product</Link> <br/>
+      <button onClick={clickHandler}>Place Order</button>
+    </>
+  );
 }
 
 export default Home;
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,12 +1,18 @@
-function ProductList() {
+import Link from "next/link";
+
+function ProductList(id = 1) {
   return (
     <>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/">Back to home</Link>
+      <br />
+      <Link href={`/product/${id}`}>Product 1</Link>
+      <br />
+      <Link href="/product/2">Product 2</Link>
+      <br />
+      <Link href="/product/3">Product 3</Link>
+      <br />
     </>
   );
 }
-
 
 export default ProductList;
